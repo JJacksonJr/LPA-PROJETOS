10ae7e46fe00503daea9eb1c0cc3b8442bb9618a
JJacksonJr <jj7872404@gmail.com>
2026-03-03 21:59:03 -0300

    Add files via upload

---
+++ Maior_Menor.py
@@ -0,0 +1,21 @@
+import os  
+os.system("cls||clean")
+
+N1=int(input(" 1=Digite um Numero  "))
+N2=int(input(" 2=Digite um Numero "))
+
+if N1>N2:
+    print(" Numero 1 é maior")
+elif N2>N1:
+    print(" Numero 2 é maior ")
+
+else:
+    print(" Não existem valor maior, os dois são iguais ")
+
+    # Ou if N1>N2:
+    #print(" Numero 1 é maior")
+#elif N1==N2:
+   # print(" Não existem valor maior, os dois são iguais ")
+
+#else:
+   # print(" Numero 2 é maior")
